2875498ef7e521ef6e3c6fa4b5ecf24a395e83d8
Andy Stiff <andy@stiffcompetitionart.com>
2026-07-03 12:54:59 +1000

    Add files via upload

--- index.js
+++ index.js
@@ -242,8 +242,9 @@ app.post("/claim/:token/submit", express.json(), async (req, res) => {
     const metafieldsData = await metafieldsResponse.json();
 
     const metafields = metafieldsData.metafields;
-    const getMeta = (key) => {
-      const field = metafields.find((m) => m.namespace === "verisart" && m.key === key);
+    console.log("ALL METAFIELDS:", JSON.stringify(metafields, null, 2));
+    const getMeta = (key, namespace = "verisart") => {
+      const field = metafields.find((m) => m.namespace === namespace && m.key === key);
       return field ? field.value : "";
     };
 
@@ -255,14 +256,14 @@ app.post("/claim/:token/submit", express.json(), async (req, res) => {
       description: productData.product.body_html.replace(/<[^>]*>/g, ''),
       image: ipfsImageUrl,
       attributes: [
-        { trait_type: "Character", value: getMeta("character") },
-        { trait_type: "Theme", value: getMeta("gimmick") },
+        { trait_type: "Character", value: getMeta("character", "custom") },
+        { trait_type: "Theme", value: getMeta("gimmick", "custom") },
         { trait_type: "Collection", value: getMeta("inspection_grade") },
         { trait_type: "Structural Rigidity", value: getMeta("structural_rigidity") },
         { trait_type: "Innuendo Intensity", value: getMeta("innuendo_intensity") },
         { trait_type: "Friction Force", value: getMeta("friction_force") },
         { trait_type: "Tactical Girth", value: getMeta("tactical_girth") },
-        { trait_type: "Lore", value: getMeta("expanded_lore") },
+        { trait_type: "Lore", value: getMeta("expanded_lore", "custom") },
       ],
     };
 
